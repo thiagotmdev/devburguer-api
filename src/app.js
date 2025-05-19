3b160c826fa7c976170ae9b5dd76a28b308fc27a
Thiago Mendes <thiagomendes.dev@gmail.com>
2025-05-19 11:40:14 -0300

    utilização do cors no server

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@
 import express from 'express';
 import routes from './routes';
 import { resolve } from 'node:path';
+import cors from 'cors';
 
 import './database';
 
 class App {
     constructor() {
         this.app = express();
-
+        this.app.use(cors());
         this.middlewares();
         this.routes();
     }
